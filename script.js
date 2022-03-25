7526dbd0a65cb487aff5609b7f44ec8aabd19d29
sergiomendozer <sergiomendozansx@gmail.com>
2022-03-25 11:55:28 -0700

    start

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //
+// function cal_loan(){
+
+// }
+var amount = parseFloat(100);
+var n_payments = parseFloat(2);
+var rate = parseFloat(3.1) / 100;
+var payment = 1;
+
+console.log(amount, n_payments, rate);
+console.log(rate);
