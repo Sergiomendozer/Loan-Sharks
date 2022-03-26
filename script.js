eba2ca928f9f39c05fe158872a86a9e6e1bbe865
sergiomendozer <sergiomendozansx@gmail.com>
2022-03-25 19:33:23 -0700

    total loan

--- script.js
+++ script.js
@@ -2,9 +2,9 @@
 // function cal_loan(){
 
 // }
-var amount = parseFloat(100);
-var months = parseFloat(2);
-var rate = parseFloat(3.1) / 100;
+var amount = parseFloat(5000);
+var months = parseFloat(12);
+var rate = parseFloat(6.0) / 100;
 var monthly_rate = rate / 12;
 // console.log(monthly_rate);
 //
@@ -16,22 +16,12 @@ console.log(p);
 p = Math.round(p * 100) / 100;
 console.log(p);
 payment = p.toFixed(2); // 3.14 returns a string
-console.log(payment);
 
-// var decimal = p.indexOf(".");
-// console.log(decimal);
+var total_principle = amount;
+var total_cost = String(months * parseFloat(payment));
+var total_interest = String(parseFloat(total_cost) - amount);
 
-// var n_round = p[decimal + 3];
-// console.log(n_round);
-// if (n_round > 4) {
-//   console.log("called");
-//   console.log(p[decimal + 2]);
-//   p[decimal + 2] = 0;
-//   console.log(p[decimal + 2]);
-
-//   //   p[decimal + 1] = parseFloat(p[decimal + 1]) + 1;
-// }
-console.log(p);
-
-// var payment = console.log(amount, rate);
-// console.log(rate);
+console.log("monthly payment: " + payment);
+console.log();
+console.log("total cost: " + total_cost);
+console.log(months);
