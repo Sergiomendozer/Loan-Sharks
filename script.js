5731b54eaa50afc3af6d1ec60a5b6850da4a5741
sergiomendozer <sergiomendozansx@gmail.com>
2022-03-27 11:44:39 -0700

    more accurate total loan adn total interest

--- script.js
+++ script.js
@@ -10,27 +10,36 @@ var principle = parseFloat(5000);
 var months = parseFloat(12);
 var rate = parseFloat(6.0) / 100;
 var monthly_rate = rate / 12;
-// console.log(monthly_rate);
-//
+
+//! above it the inputted values
 var numerator = principle * monthly_rate * Math.pow(1 + monthly_rate, months);
 var denominator = Math.pow(1 + monthly_rate, months) - 1;
-var z = numerator / denominator;
+var payment = numerator / denominator;
 
-p = Math.round(z * 100) / 100;
-payment = p.toFixed(2);
+payment_rounded = Math.round(payment * 100) / 100;
+var payment_rounded = payment_rounded.toFixed(2);
 
 var total_principle = principle;
-var total_loan = String(months * parseFloat(payment));
-var total_interest = String(parseFloat(total_loan) - principle);
-total_interest = Math.round(total_interest * 100) / 100;
-total_interest = total_interest.toFixed(2);
+var total_loan = months * parseFloat(payment);
+var total_interest = parseFloat(total_loan) - principle;
+// total_interest = Math.round(total_interest * 100) / 100;
+// total_interest = total_interest.toFixed(2);
 
 var percentage_principle = (principle / total_loan) * 100;
 var percentage_interest = (total_interest / total_loan) * 100;
+
+// function makes sure number is not rounded up
+function toFixed(num, fixed) {
+  var re = new RegExp("^-?\\d+(?:.\\d{0," + (fixed || -1) + "})?");
+  return num.toString().match(re)[0];
+}
+total_loan = toFixed(total_loan, 2);
+total_interest = toFixed(total_interest, 2);
+// total_loan = total_loan.toFixed(2);
 percentage_principle = percentage_principle.toFixed(2) + "%";
 percentage_interest = percentage_interest.toFixed(2) + "%";
 
-console.log("Monthly Payment: $" + payment);
+console.log("Monthly Payment: $" + payment_rounded);
 console.log("Total Principle: $" + principle);
 console.log("Total Interest: $" + total_interest);
 console.log("Total Loan: $" + total_loan);
@@ -68,7 +77,7 @@ for (var i = 1; i < months + 1; i++) {
 
 for (var a = 1; a < months + 1; a++) {
   loan_info_table.push(column[a]);
-  loan_info_table[a].push("$" + payment);
+  loan_info_table[a].push("$" + payment_rounded);
 }
 
 // math.rounds after all calculations
@@ -78,16 +87,17 @@ var r_balance = principle;
 var interest_paid = 0;
 for (var b = 1; b < months + 1; b++) {
   var interest_amount = monthly_rate * r_balance;
-  var c_principle = z - interest_amount;
+  var c_principle = payment - interest_amount;
   interest_paid = interest_paid + interest_amount;
   r_balance = r_balance - c_principle;
 
-  // rounds up numbers
+  // rounds up numbers before pushing to table
   c_principle = Math.round(c_principle * 100) / 100;
   interest_amount = Math.round(interest_amount * 100) / 100;
   interest_paid = Math.round(interest_paid * 100) / 100;
   r_balance = Math.round(r_balance * 100) / 100;
 
+  // pushing values to table
   loan_info_table[b].push("$" + c_principle);
   loan_info_table[b].push("$" + interest_amount);
   loan_info_table[b].push("$" + interest_paid);
