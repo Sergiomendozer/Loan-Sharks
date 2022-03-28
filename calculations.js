32f2aa9ffff92e00115d246de3194e9f2e306187
sergiomendozer <sergiomendozansx@gmail.com>
2022-03-27 18:17:41 -0700

    deleted comments

--- calculations.js
+++ calculations.js
@@ -4,8 +4,6 @@ var months = parseFloat(12);
 var rate = parseFloat(6.0) / 100;
 //? start of calculations
 
-//! next last payment can be diff can be more or less
-//! above it the inputted values
 var monthly_rate = rate / 12;
 var numerator = principle * monthly_rate * Math.pow(1 + monthly_rate, months);
 var denominator = Math.pow(1 + monthly_rate, months) - 1;
@@ -17,22 +15,12 @@ var payment_rounded = payment_rounded.toFixed(2);
 var total_principle = principle;
 var total_loan = months * parseFloat(payment);
 var total_interest = parseFloat(total_loan) - principle;
-// total_interest = Math.round(total_interest * 100) / 100;
-// total_interest = total_interest.toFixed(2);
 
 var percentage_principle = (principle / total_loan) * 100;
 var percentage_interest = (total_interest / total_loan) * 100;
 
-// function makes sure number is not rounded up
-// function toFixed(num, fixed) {
-//   var re = new RegExp("^-?\\d+(?:.\\d{0," + (fixed || -1) + "})?");
-//   return num.toString().match(re)[0];
-// }
 total_loan = Math.round(total_loan * 100) / 100;
-// total_loan = toFixed(total_loan, 2);
-// total_interest = toFixed(total_interest, 2);
 total_interest = Math.round(total_interest * 100) / 100;
-// total_loan = total_loan.toFixed(2);
 percentage_principle = percentage_principle.toFixed(2) + "%";
 percentage_interest = percentage_interest.toFixed(2) + "%";
 
@@ -69,17 +57,7 @@ for (var a = 1; a < months + 1; a++) {
   loan_info_table[a].push("$" + payment_rounded);
 }
 
-// math.rounds after all calculations
-// b.push together//! console.log(column[1]); for testing
-//! console.log(column[2]);
-//! console.log(column[3]);
-//! console.log(column[4]);
-//! console.log(column[5]);
-//! console.log(column[6]);
-//! console.log(column[11]);
-//! console.log(column[12]); for testing
 var remaining_balance = principle;
-// console.log(remaining_balance);
 var interest_paid = 0;
 for (var b = 1; b < months + 1; b++) {
   var interest_amount = monthly_rate * remaining_balance;
@@ -99,13 +77,5 @@ for (var b = 1; b < months + 1; b++) {
   loan_info_table[b].push("$" + interest_paid_rounded);
   loan_info_table[b].push("$" + remaining_balance_rounded);
 }
-// console.log(loan_info_table[months][5]);
-// console.log();
-// if (loan_info_table[months][5] == "$0") {
-//   console.table(loan_info_table);
-// } else {
-//   console.log("called");
-//   console.table(loan_info_table);
-// }
+
 console.table(loan_info_table);
-//! make sure at the top all rounding is together
---
+++ next.js
@@ -0,0 +1,4 @@
+//TODO: start
+//TODO: deletes some code
+//TODO: add comments
+//TODO: create html hello world file for test
