a72f0f8e27cdc4882a56d51a839853a20ca8a0b5
sergiomendozer <sergiomendozansx@gmail.com>
2022-03-28 16:41:18 -0700

    grabs all values in one function

--- scripts.js
+++ scripts.js
@@ -1,25 +1,15 @@
 var principle = null;
 var months = null;
 var rate = null;
-function get_principle(event) {
-  principle = parseFloat(document.getElementById("principle").value);
-  console.log(principle);
-  console.log("principle value"); //!for test
-}
-
-function get_months(event) {
-  months = parseFloat(document.getElementById("months").value);
-  console.log(months);
-  console.log("months value"); //!for test
-}
-
-function get_rate(event) {
-  rate = parseFloat(document.getElementById("rate").value);
-  console.log(rate);
-  console.log("rate value"); //!for test
-}
 
 function calculations() {
-  console.log("start");
+  console.log("calculations");
+  principle = parseFloat(document.getElementById("principle").value);
+  months = parseFloat(document.getElementById("months").value);
   rate = parseFloat(document.getElementById("rate").value);
+  if (principle != null && months != null && rate != null) {
+    console.log("P: " + principle); //!for testing
+    console.log("#: " + months); //!for testing
+    console.log("rate: " + rate); //!for testing
+  }
 }
