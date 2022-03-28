5c4b72f6849a084c40657fe95d404a86cc81145e
sergiomendozer <sergiomendozansx@gmail.com>
2022-03-28 16:33:09 -0700

    grabs values in input boxes

--- scripts.js
+++ scripts.js
@@ -1,11 +1,25 @@
 var principle = null;
-function get_value(event) {
-  principle = document.getElementById("loan_amount").value;
+var months = null;
+var rate = null;
+function get_principle(event) {
+  principle = parseFloat(document.getElementById("principle").value);
   console.log(principle);
-  console.log("loan_amount"); //!for test
+  console.log("principle value"); //!for test
 }
-function start() {
+
+function get_months(event) {
+  months = parseFloat(document.getElementById("months").value);
+  console.log(months);
+  console.log("months value"); //!for test
+}
+
+function get_rate(event) {
+  rate = parseFloat(document.getElementById("rate").value);
+  console.log(rate);
+  console.log("rate value"); //!for test
+}
+
+function calculations() {
   console.log("start");
-  var val = document.getElementById("loan_amount").value;
-  console.log(val);
+  rate = parseFloat(document.getElementById("rate").value);
 }
