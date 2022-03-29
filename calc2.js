08fbd9da63da3ed3cc720be4b284aa068af9e34f
sergiomendozer <sergiomendozansx@gmail.com>
2022-03-28 18:37:59 -0700

    test button and values pop up

---
+++ calc2.js
@@ -0,0 +1,93 @@
+// ? test 3 pass
+var principle = parseFloat(5000);
+var months = parseFloat(12);
+var rate = parseFloat(6.0) / 100;
+//? start of calculations
+
+// payment calculations
+var monthly_rate = rate / 12;
+var numerator = principle * monthly_rate * Math.pow(1 + monthly_rate, months);
+var denominator = Math.pow(1 + monthly_rate, months) - 1;
+var payment = numerator / denominator;
+//payment rounded calculation
+payment_rounded = Math.round(payment * 100) / 100;
+var payment_rounded = payment_rounded.toFixed(2);
+
+var total_principle = principle;
+var total_loan = months * parseFloat(payment);
+var total_interest = parseFloat(total_loan) - principle;
+
+//percentage calculations
+var percentage_principle = (principle / total_loan) * 100;
+var percentage_interest = (total_interest / total_loan) * 100;
+
+// rounded up numbers
+total_loan = Math.round(total_loan * 100) / 100;
+total_interest = Math.round(total_interest * 100) / 100;
+percentage_principle = percentage_principle.toFixed(2) + "%";
+percentage_interest = percentage_interest.toFixed(2) + "%";
+
+console.log("Monthly Payment: $" + payment_rounded);
+console.log("Total Principle: $" + principle);
+console.log("Total Interest: $" + total_interest);
+console.log("Total Loan: $" + total_loan);
+console.log(" "); // empty to separate
+console.log("Principle Percentage: " + percentage_principle);
+console.log("Interest Percentage: " + percentage_interest);
+
+// main array that will be displayed for user and contains loan information
+var loan_info_table = [
+  [
+    "Month",
+    "Payment",
+    "Principle",
+    "Interest amount",
+    "Interest paid",
+    "Remaining Balance",
+  ],
+];
+
+// for loop creates n number amount of arrays
+//[n] this is created until it
+var column = [];
+for (var n = 1; n < months + 1; n++) {
+  column[n] = "empty";
+}
+column;
+
+//for loop turns integer into string in arrays created above
+for (var i = 1; i < months + 1; i++) {
+  column[i] = Object.assign([String(i)], i);
+}
+
+// for loop if for appending/pushing loan info elements
+for (var a = 1; a < months + 1; a++) {
+  loan_info_table.push(column[a]);
+  loan_info_table[a].push("$" + payment_rounded);
+}
+
+//for loop calculates current_principle, interest_amount,interest_paid_rounded,remaining_balance
+//for loop also pushes/appends result of calculations to main array loan_info_table
+var remaining_balance = principle;
+var interest_paid = 0;
+for (var b = 1; b < months + 1; b++) {
+  //Calculations
+  var interest_amount = monthly_rate * remaining_balance;
+  var c_principle = payment - interest_amount;
+  interest_paid = interest_paid + interest_amount;
+  remaining_balance = remaining_balance - c_principle;
+
+  // rounds up numbers before pushing to table
+  c_principle = Math.round(c_principle * 100) / 100;
+  interest_amount = Math.round(interest_amount * 100) / 100;
+  interest_paid_rounded = Math.round(interest_paid * 100) / 100;
+  var remaining_balance_rounded = Math.round(remaining_balance * 100) / 100;
+
+  // pushing values to table
+  loan_info_table[b].push("$" + c_principle);
+  loan_info_table[b].push("$" + interest_amount);
+  loan_info_table[b].push("$" + interest_paid_rounded);
+  loan_info_table[b].push("$" + remaining_balance_rounded);
+}
+
+console.table(loan_info_table);
--- scripts.js
+++ scripts.js
@@ -2,18 +2,66 @@ var principle = null;
 var months = null;
 var rate = null;
 
+function test() {
+  document.getElementById("principle").value = 5000;
+  document.getElementById("months").value = 60;
+  document.getElementById("rate").value = 6;
+}
+
 function calculations() {
   console.log("calculations");
   principle = parseFloat(document.getElementById("principle").value);
   months = parseFloat(document.getElementById("months").value);
   rate = parseFloat(document.getElementById("rate").value);
   if (
-    isNaN(principle) == false &&
-    isNaN(months) == false &&
+    isNaN(principle) == false ||
+    isNaN(months) == false ||
     isNaN(rate) == false
   ) {
     console.log("P: " + principle); //!for testing
     console.log("#: " + months); //!for testing
     console.log("rate: " + rate); //!for testing
+    /////////////////////////////////////
+    // payment calculations
+    var monthly_rate = rate / 12;
+    var numerator =
+      principle * monthly_rate * Math.pow(1 + monthly_rate, months);
+    var denominator = Math.pow(1 + monthly_rate, months) - 1;
+    var payment = numerator / denominator;
+    //payment rounded calculation
+    payment_rounded = Math.round(payment * 100) / 100;
+    var payment_rounded = "$" + payment_rounded.toFixed(2);
+
+    var total_principle = principle;
+    var total_loan = months * parseFloat(payment);
+    var total_interest = parseFloat(total_loan) - principle;
+
+    //percentage calculations
+    var percentage_principle = (principle / total_loan) * 100;
+    var percentage_interest = (total_interest / total_loan) * 100;
+
+    // rounded up numbers
+    total_loan = "$" + Math.round(total_loan * 100) / 100;
+    total_interest = "$" + Math.round(total_interest * 100) / 100;
+    percentage_principle = percentage_principle.toFixed(2) + "%";
+    percentage_interest = percentage_interest.toFixed(2) + "%";
+    principle = "$" + principle;
+
+    document.getElementById("monthly_payments").innerHTML =
+      "Monthly Payment:" + payment_rounded;
+    document.getElementById("total_principle").innerHTML =
+      "Total Principle:" + principle;
+    document.getElementById("total_interest").innerHTML =
+      "Total Interest:" + total_interest;
+    document.getElementById("total_loan").innerHTML =
+      "Total Loan:" + total_loan;
+
+    console.log("Monthly Payment:" + payment_rounded); //!for testing
+    console.log("Total Principle:" + principle); //!for testing
+    console.log("Total Interest:" + total_interest); //!for testing
+    console.log("Total Loan:" + total_loan); //!for testing
+    console.log(" "); // empty to separate
+    console.log("Principle Percentage: " + percentage_principle); //!for testing
+    console.log("Interest Percentage: " + percentage_interest); //!for testing
   }
 }
