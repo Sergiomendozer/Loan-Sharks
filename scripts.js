279456b89ef43d595437e8e17cc1d3270a8919b0
sergiomendozer <sergiomendozansx@gmail.com>
2022-03-30 11:00:16 -0700

    interest paidis added to html table

--- scripts.js
+++ scripts.js
@@ -183,6 +183,22 @@ function calculations() {
       const id_interest_amount = document.getElementById("Interest amount" + c);
       id_interest_amount.appendChild(Tag_row_interest_amount);
 
+      // Interest paid append
+      const table_tag_Interest_paid = document.createElement("tr");
+      table_tag_Interest_paid.setAttribute("Id", "Interest paid" + c);
+      const empty_element_Interest_paid = document.createTextNode("");
+      table_tag_Interest_paid.appendChild(empty_element_Interest_paid);
+      const doc_Interest_paid = document.getElementById("Interest paid row");
+      doc_Interest_paid.appendChild(table_tag_Interest_paid);
+
+      const Tag_row_Interest_paid = document.createElement("tr");
+      const Interest_paid_i = document.createTextNode(
+        "$" + interest_paid_rounded
+      );
+      Tag_row_Interest_paid.appendChild(Interest_paid_i);
+      const id_Interest_paid = document.getElementById("Interest paid" + c);
+      id_Interest_paid.appendChild(Tag_row_Interest_paid);
+
       // pushing values to table
       //   loan_info_table[b].push("$" + c_principle);
       //   loan_info_table[b].push("$" + interest_amount);
